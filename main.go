f5bad62e1747805ee368f6f2c57f55e081fc4173
iamwill <19693587+iamwillzhu@users.noreply.github.com>
2022-12-24 23:04:55 -0500

    feat: add discord interaction response

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Request struct {
-	ID    float64 `json:"id"`
-	Value string  `json:"value"`
+// InteractionResponseType is type of interaction response.
+type InteractionResponseType uint8
+
+// Interaction response types.
+const (
+	// InteractionResponsePong is for ACK ping event.
+	InteractionResponsePong InteractionResponseType = 1
+	// InteractionResponseChannelMessageWithSource is for responding with a message, showing the user's input.
+	InteractionResponseChannelMessageWithSource InteractionResponseType = 4
+	// InteractionResponseDeferredChannelMessageWithSource acknowledges that the event was received, and that a follow-up will come later.
+	InteractionResponseDeferredChannelMessageWithSource InteractionResponseType = 5
+	// InteractionResponseDeferredMessageUpdate acknowledges that the message component interaction event was received, and message will be updated later.
+	InteractionResponseDeferredMessageUpdate InteractionResponseType = 6
+	// InteractionResponseUpdateMessage is for updating the message to which message component was attached.
+	InteractionResponseUpdateMessage InteractionResponseType = 7
+	// InteractionApplicationCommandAutocompleteResult shows autocompletion results. Autocomplete interaction only.
+	InteractionApplicationCommandAutocompleteResult InteractionResponseType = 8
+	// InteractionResponseModal is for responding to an interaction with a modal window.
+	InteractionResponseModal InteractionResponseType = 9
+)
+
+// InteractionResponse represents a response for an interaction event.
+type InteractionResponse struct {
+	Type InteractionResponseType `json:"type,omitempty"`
 }
 
-type Response struct {
-	Message string `json:"message"`
-	Ok      bool   `json:"ok"`
-}
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-func Handler(request Request) (Response, error) {
-	return Response{
-		Message: fmt.Sprintf("Process Request ID %f", request.ID),
-		Ok:      true,
+	interactionResponse := InteractionResponse{
+		Type: InteractionResponsePong,
+	}
+
+	interactionResponseStr, err := json.Marshal(interactionResponse)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 401,
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(interactionResponseStr),
 	}, nil
 }
 
