e824b4e34945b5d64b6702aa326d546be9cf4d55
iamwill <19693587+iamwillzhu@users.noreply.github.com>
2022-12-25 02:15:40 -0500

    feat: add signature verification to webhook messages

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -33,23 +41,82 @@ type InteractionResponse struct {
 	Type InteractionResponseType `json:"type,omitempty"`
 }
 
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// VerifyInteraction implements message verification of the discord interactions api
+// signing algorithm, as documented here:
+// https://discord.com/developers/docs/interactions/receiving-and-responding#security-and-authorization
+func VerifyInteraction(r *events.APIGatewayProxyRequest, key ed25519.PublicKey) bool {
+	var msg bytes.Buffer
+
+	signature, ok := r.Headers["X-Signature-Ed25519"]
+	if !ok || signature == "" {
+		return false
+	}
+
+	sig, err := hex.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+
+	if len(sig) != ed25519.SignatureSize {
+		return false
+	}
+
+	timestamp, ok := r.Headers["X-Signature-Timestamp"]
+	if !ok || timestamp == "" {
+		return false
+	}
+
+	msg.WriteString(timestamp)
+
+	var body bytes.Buffer
+	bodyReader := strings.NewReader(r.Body)
+
+	// copy body into buffers
+	_, err = io.Copy(&msg, io.TeeReader(bodyReader, &body))
+	if err != nil {
+		return false
+	}
+
+	return ed25519.Verify(key, msg.Bytes(), sig)
+}
+
+func Handler(request *events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse {
+
+	log.Printf("request: %v", request)
+
+	discordPublicKey := os.Getenv("DISCORD_PUBLIC_KEY")
+
+	if discordPublicKey == "" {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Internal Error: discord public key is not found",
+		}
+	}
+
+	key, err := hex.DecodeString(discordPublicKey)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Internal Error: %s", err.Error()),
+		}
+	}
+
+	if ok := VerifyInteraction(request, key); !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 401,
+		}
+	}
 
 	interactionResponse := InteractionResponse{
 		Type: InteractionResponsePong,
 	}
 
-	interactionResponseStr, err := json.Marshal(interactionResponse)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 401,
-		}, err
-	}
+	interactionResponseStr, _ := json.Marshal(interactionResponse)
 
-	return events.APIGatewayProxyResponse{
+	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       string(interactionResponseStr),
-	}, nil
+	}
 }
 
 func main() {
